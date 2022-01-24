fda726115332b5ffd0ab84f1c54459d66e247003
Vakor <myksv2003@gmail.com>
2022-01-24 20:32:48 +0200

    Collection preview added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import {Route} from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shoppage/shoppage.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path={'/'} component={HomePage}/>
+      <Route exact path={'/shop'} component={ShopPage}/>
     </div>
   );
 }
