1cf564d73d63f940a124627e65db34208e989a72
Vakor <myksv200@gmail.com>
2022-02-08 11:02:11 +0200

    Updated cart, added addItem func

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,15 +1,22 @@
 import CartActionTypes from "./cart.types";
+import {addItemToCart} from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
+    cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type){
+    switch (action.type) {
         case CartActionTypes.TOGGLE_CARD_HIDDEN:
-            return{
+            return {
                 ...state,
                 hidden: !state.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload),
             }
         default:
             return state;
